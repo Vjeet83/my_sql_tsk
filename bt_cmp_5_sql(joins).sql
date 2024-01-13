1c5693eb7f3fb0b19fb1b0975688b15cc3a69408
Vjeet83 <134676759+Vjeet83@users.noreply.github.com>
2024-01-13 20:11:32 +0530

    Add files via upload

---
+++ bt_cmp_5_sql(joins).sql
@@ -0,0 +1,20 @@
+#   joins
+-- relational database ke lie keeys chahie or 
+-- keys kam me lene k lie joits mangta h 
+-- 1 inner join (joins )
+-- 2 Outer join 
+-- 3 left join 
+-- 4 right join
+-- 5
+
+use world ;
+desc city;
+desc country;
+
+
+
+
+# read about types of subquary
+
+
+
